566a98cfc79bb5bd87d7229ebb524d7e724976c0
Aparna venugopal <aparnavenugopal1996@gmail.com>
2024-05-06 20:23:06 +0530

    code change 9

--- 12-adv-starting-project/frontend/src/pages/Events.js
+++ 12-adv-starting-project/frontend/src/pages/Events.js
@@ -5,6 +5,10 @@ import { useLoaderData } from 'react-router-dom';
 
 function EventsPage() {
   const events = useLoaderData();
+
+  if(events.isError){
+    return <p>{events.message}</p>
+  }
   
  
   return (
@@ -15,3 +19,13 @@ function EventsPage() {
 }
 
 export default EventsPage;
+
+export async function loader(){
+    const response = await fetch('http://localhost:8080/events');
+    if(!response.ok){
+      return {isError: true, message:'could not fetch events'}
+    }else{
+     const resData = await response.json();
+     return resData.events;
+    }
+ }
--- 12-adv-starting-project/frontend/src/pages/Root.js
+++ 12-adv-starting-project/frontend/src/pages/Root.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import MainNavigation from '../components/MainNavigation';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useNavigation } from 'react-router-dom';
 
 const Root = () => {
+  const navigation = useNavigation();
   return (
     <div>
         <MainNavigation/>
         <main>
+            {/* {navigation.state === 'loading' && <p>loading...</p>} */}
             <Outlet/>
         </main>
     </div>
